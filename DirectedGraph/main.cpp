ddd6d3cb287655552ce902885c3d71e98feee545
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-03-14 21:13:01 -0500

    Adds code for Sedgewick topological sort digraph

--- DirectedGraph/main.cpp
+++ DirectedGraph/main.cpp
@@ -27,5 +27,32 @@ int main(int argc, const char * argv[]) {
     l.RecursiveDFS();
     cout << endl;
     l.PrintEdgeTo();
+    
+    /**
+         Digraph l(13);
+         l.AddEdge(0,1);
+         l.AddEdge(0,5);
+         l.AddEdge(0,6);
+         l.AddEdge(2,0);
+         l.AddEdge(2,3);
+         l.AddEdge(3,5);
+         l.AddEdge(5,4);
+         l.AddEdge(6,4);
+         l.AddEdge(6,9);
+         l.AddEdge(7,6);
+         l.AddEdge(8,7);
+         l.AddEdge(9,10);
+         l.AddEdge(9,11);
+         l.AddEdge(9,12);
+         l.AddEdge(11,12);
+         l.Print();
+         cout << endl;
+         l.RecursiveDFS();
+         cout << endl;
+     //    l.PrintEdgeTo();
+         return 0;
+     
+     */
+    
     return 0;
 }
