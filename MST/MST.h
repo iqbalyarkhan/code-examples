e88bfc9b4f3664d4b767c7be40908c81b2b92b54
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-03-23 18:48:32 -0500

    Adds skeleton MST

--- MST/MST.h
+++ MST/MST.h
@@ -9,5 +9,37 @@
 #ifndef MST_h
 #define MST_h
 
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <vector>
+#include 
+
+using namespace std;
+
+class MST{
+private:
+    //From UnionFind implementation
+    vector<int> connectionsArray;
+    vector<int> children;
+    int numberOfSites;
+    
+    //From heaps implementation
+    vector<int> heapArr;
+    void trickleUp(int item);
+    void trickleDown(int item);
+    
+    
+public:
+    //From UnionFind Implementation
+    void Union(int,int);
+    int FindRoot(int);
+    void Unionize(int,int);
+    
+    //From heaps implementation
+    void Insert(int item);
+    int GetMin();
+    
+};
 
 #endif /* MST_h */
