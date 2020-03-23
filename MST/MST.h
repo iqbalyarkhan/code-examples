51c2fa44c788f29917e5fb4fa8078de29453c81c
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-03-23 18:48:00 -0500

    Adds skeleton setup for MST

---
+++ MST/MST.h
@@ -0,0 +1,13 @@
+//
+//  MST.h
+//  Test_UVA
+//
+//  Created by Iqbal Khan on 3/23/20.
+//  Copyright © 2020 Iqbal Khan. All rights reserved.
+//
+
+#ifndef MST_h
+#define MST_h
+
+
+#endif /* MST_h */
---
+++ MST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MST
+//
+//  Created by Iqbal Khan on 3/23/20.
+//  Copyright © 2020 Iqbal Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
