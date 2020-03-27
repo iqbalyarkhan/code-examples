f3dd0b1912f1b07681745e12c0253aee2cc637fa
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-03-26 19:07:09 -0500

    Adds code for MST

--- MST/MST.h
+++ MST/MST.h
@@ -13,11 +13,15 @@
 #include <string>
 #include <stdio.h>
 #include <vector>
+#include <utility>
 
 using namespace std;
 
 class MST{
 private:
+    //From weighted undirected graph
+    vector<vector<pair<int,int>>> graph;
+    
     //From UnionFind implementation
     vector<int> connectionsArray;
     vector<int> children;
@@ -30,6 +34,11 @@ private:
     
     
 public:
+    //From weighted undirected graph
+    int GetNumberOfVertices();
+    void Insert(int,int, int);
+    void Print();
+    
     //From UnionFind Implementation
     void Union(int,int);
     int FindRoot(int);
@@ -47,10 +56,28 @@ MST::MST(int v) : numberOfSites(v){
     for (int i = 0; i < numberOfSites; i++)
         connectionsArray.push_back(i);
     children.resize(numberOfSites);
-    
-    for (int i = 0; i < numberOfSites; i++){
-        cout << connectionsArray[i] << " " << children[i] << endl;
+    graph.resize(numberOfSites);
+}
+
+void MST::Insert(int from, int to, int weight){
+    pair<int,int> edge1(from, weight);
+    pair<int,int> edge2(to,weight);
+    graph[from].push_back(edge2);
+    graph[to].push_back(edge1);
+}
+
+void MST::Print(){
+    cout << endl;
+    for (int i = 0; i < graph.size(); i++){
+        cout << i;
+        for (int j = 0; j < graph[i].size(); j++){
+            cout << " -> ";
+            cout << "(" << graph[i][j].first << ", ";
+            cout << graph[i][j].second << ")";
+        }
+        cout << endl;
     }
+    cout << endl;
 }
 
 #endif /* MST_h */
--- MST/main.cpp
+++ MST/main.cpp
@@ -10,7 +10,17 @@
 #include "MST.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    MST mst(10);
+    MST mst(7);
+    mst.Insert(0, 1, 4);
+    mst.Insert(0, 2, 3);
+    mst.Insert(1, 2, 1);
+    mst.Insert(1, 3, 2);
+    mst.Insert(2, 3, 3);
+    mst.Insert(3, 4, 5);
+    mst.Insert(3, 5, 7);
+    mst.Insert(4, 5, 6);
+    mst.Insert(4, 6, 1);
+    mst.Insert(5, 6, 1);
+    mst.Print();
     return 0;
 }
