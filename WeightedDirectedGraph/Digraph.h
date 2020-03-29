bebf227367393d14972c85df1f4aa9b339e807dd
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-03-29 18:18:07 -0500

    Adds skeleton for weighted directed graphs

--- WeightedDirectedGraph/Digraph.h
+++ WeightedDirectedGraph/Digraph.h
@@ -20,7 +20,7 @@ using namespace std;
 class Digraph{
 private:
     int vectorSize;
-    vector<vector<pair<int,int>>> adjList;
+    vector<vector<pair<int,double>>> adjList;
     vector<bool> visited;
     vector<int> edgeTo;
     queue<int> pre;
@@ -32,7 +32,7 @@ private:
     
 public:
     Digraph(int);
-    void AddEdge(int,int);
+    void Insert(int,int,double);
     void Print();
     void BFS(int);
     void RecursiveDFS();
@@ -42,110 +42,110 @@ public:
     void PrintOrder();
 };
 
-void Digraph::PrintOrder(){
-    cout << "Pre: " << endl;
-    while (!pre.empty()){
-        cout << pre.front() << " ";
-        pre.pop();
-    }
-    cout << endl;
-    cout << "Post: " << endl;
-    while (!post.empty()){
-        cout << post.front() << " ";
-        post.pop();
-    }
-    cout << endl;
-    cout << "Reverse Post: " << endl;
-    while (!reversePost.empty()){
-        cout << reversePost.top() << " ";
-        reversePost.pop();
-    }
-    cout << endl;
-}
-
-void Digraph::RecursiveDFS(){
-    for (int i = 0; i < visited.size(); i++){
-        if (!visited[i]){
-            RecursiveDFS(i);
-        }
-    }
-}
-
-void Digraph::RecursiveDFS(int v){
-    visited[v] = true;
-    pre.push(v);
-    for (int i = 0; i < adjList[v].size(); i++){
-        int curr = adjList[v][i];
-        if (!visited[curr]){
-            edgeTo[curr] = v;
-            RecursiveDFS(curr);
-        }
-    }
-    post.push(v);
-    reversePost.push(v);
-}
-
-void Digraph::FindPath(int v, int w){
-    /**
-     Begin at the w vertex: 5 (push 5 on the stack)
-     Check edgeTo[w] = edgeTo[5] = 3. (push 3 on the stack)
-     Go to edgeTo[3] = 2 (push 2 on the stack)
-     Go to edgeTo[2] = 0 (push 0 on the stack)
-     We're now at v vertex: 0
-     */
-    stack<int> s;
-    int end = w;
-    while (true){
-        s.push(end);
-        if (end == v){
-            break;
-        }
-        end = edgeTo[end];
-    }
-    
-    cout << "Path from " << v << " to " << w <<": "<<endl;
-    while (!s.empty()) {
-        int curr = s.top();
-        cout << curr << " ";
-         s.pop();
-    }
-    
-    cout << endl;
-}
-
-void Digraph::BFS(int v){
-    vector<int> marked(vectorSize);
-    queue<int> q;
-    marked[v] = true;
-    q.push(v);
-    while (!q.empty()){
-        int curr = q.front();
-        cout << curr << " ";
-        q.pop();
-        for (int i = 0; i < adjList[curr].size(); i++){
-            int neighbor = adjList[curr][i];
-            if (!marked[neighbor]){
-                edgeTo[neighbor] = curr;
-                marked[neighbor] = true;
-                q.push(neighbor);
-            }
-        }
-    }
-    cout << endl;
-}
-
-void Digraph::PrintEdgeTo(){
-    for (int i = 0; i < edgeTo.size(); i++){
-        cout << "edgeTo[" << i << "]" << " --> " << edgeTo[i] << endl;
-    }
-}
-
-
+//void Digraph::PrintOrder(){
+//    cout << "Pre: " << endl;
+//    while (!pre.empty()){
+//        cout << pre.front() << " ";
+//        pre.pop();
+//    }
+//    cout << endl;
+//    cout << "Post: " << endl;
+//    while (!post.empty()){
+//        cout << post.front() << " ";
+//        post.pop();
+//    }
+//    cout << endl;
+//    cout << "Reverse Post: " << endl;
+//    while (!reversePost.empty()){
+//        cout << reversePost.top() << " ";
+//        reversePost.pop();
+//    }
+//    cout << endl;
+//}
+//
+//void Digraph::RecursiveDFS(){
+//    for (int i = 0; i < visited.size(); i++){
+//        if (!visited[i]){
+//            RecursiveDFS(i);
+//        }
+//    }
+//}
+//
+//void Digraph::RecursiveDFS(int v){
+//    visited[v] = true;
+//    pre.push(v);
+//    for (int i = 0; i < adjList[v].size(); i++){
+//        int curr = adjList[v][i];
+//        if (!visited[curr]){
+//            edgeTo[curr] = v;
+//            RecursiveDFS(curr);
+//        }
+//    }
+//    post.push(v);
+//    reversePost.push(v);
+//}
+//
+//void Digraph::FindPath(int v, int w){
+//    /**
+//     Begin at the w vertex: 5 (push 5 on the stack)
+//     Check edgeTo[w] = edgeTo[5] = 3. (push 3 on the stack)
+//     Go to edgeTo[3] = 2 (push 2 on the stack)
+//     Go to edgeTo[2] = 0 (push 0 on the stack)
+//     We're now at v vertex: 0
+//     */
+//    stack<int> s;
+//    int end = w;
+//    while (true){
+//        s.push(end);
+//        if (end == v){
+//            break;
+//        }
+//        end = edgeTo[end];
+//    }
+//
+//    cout << "Path from " << v << " to " << w <<": "<<endl;
+//    while (!s.empty()) {
+//        int curr = s.top();
+//        cout << curr << " ";
+//         s.pop();
+//    }
+//
+//    cout << endl;
+//}
+//
+//void Digraph::BFS(int v){
+//    vector<int> marked(vectorSize);
+//    queue<int> q;
+//    marked[v] = true;
+//    q.push(v);
+//    while (!q.empty()){
+//        int curr = q.front();
+//        cout << curr << " ";
+//        q.pop();
+//        for (int i = 0; i < adjList[curr].size(); i++){
+//            int neighbor = adjList[curr][i];
+//            if (!marked[neighbor]){
+//                edgeTo[neighbor] = curr;
+//                marked[neighbor] = true;
+//                q.push(neighbor);
+//            }
+//        }
+//    }
+//    cout << endl;
+//}
+//
+//void Digraph::PrintEdgeTo(){
+//    for (int i = 0; i < edgeTo.size(); i++){
+//        cout << "edgeTo[" << i << "]" << " --> " << edgeTo[i] << endl;
+//    }
+//}
+//
+//
 Digraph::Digraph(int vecSize) : vectorSize(vecSize){
     cout << "Number of vertices was passed as: " << vectorSize << endl;
     initializeStructures();
 }
-
+//
 //Helper function to set sizes for structures
 //used for various operations
 void Digraph::initializeStructures(){
@@ -154,21 +154,24 @@ void Digraph::initializeStructures(){
     edgeTo.resize(vectorSize);
     numberOfComponents = 0;
 }
-
+//
 void Digraph::Print(){
-    for (int i = 0; i < vectorSize; i++){
+    cout << endl;
+    for (int i = 0; i < adjList.size(); i++){
         cout << i;
-        for (auto j : adjList[i]){
+        for (int j = 0; j < adjList[i].size(); j++){
             cout << " -> ";
-            cout << j;
+            cout << "(" << adjList[i][j].first << ", ";
+            cout << adjList[i][j].second << ")";
         }
         cout << endl;
     }
     cout << endl;
 }
 
-void Digraph::AddEdge(int i, int j){
-    adjList[i].push_back(j);
+void Digraph::Insert(int from, int to, double weight){
+    pair<int,double> edge(to,weight);
+    adjList[from].push_back(edge);
 }
 
 
--- WeightedDirectedGraph/main.cpp
+++ WeightedDirectedGraph/main.cpp
@@ -13,6 +13,20 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    Digraph dg(4);
+    Digraph dg(8);
+    dg.Insert(5, 4, 0.35);
+    dg.Insert(4, 7, 0.37);
+    dg.Insert(5, 7, 0.28);
+    dg.Insert(5, 1, 0.32);
+    dg.Insert(4, 0, 0.38);
+    dg.Insert(0, 2, 0.26);
+    dg.Insert(3, 7, 0.39);
+    dg.Insert(1, 3, 0.29);
+    dg.Insert(7, 2, 0.34);
+    dg.Insert(6, 2, 0.40);
+    dg.Insert(3, 6, 0.52);
+    dg.Insert(6, 0, 0.58);
+    dg.Insert(6, 4, 0.93);
+    dg.Print();
     return 0;
 }
