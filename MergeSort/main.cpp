ee33a6c1af0d09acd9a1f533ebe0f4992eef3433
iqbalyarkhan <iqbal.khan@utdallas.edu>
2020-04-09 12:51:38 -0500

    Adds completed code

--- MergeSort/main.cpp
+++ MergeSort/main.cpp
@@ -2,37 +2,58 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+
 using namespace std;
 
-void recMergeSort(vector<int>&, int low, int high);
+void merge(vector<int>&, int lo, int mid, int hi);
 
-void mergeArrs(vector<int>& A, int low, int mid, int high);
+void recMergeSort(vector<int>&,int lo, int hi);
 
 int main(){
-    vector<int> input = {9,8,7,6,5,4,3,2,1};
-    recMergeSort(input, 0, int(input.size()));
-}
-
-void recMergeSort(vector<int>& arr, int low, int high){
-    if (high <= low)
-        return;
-    int mid = low + (high - low)/2;
-    recMergeSort(arr, low, mid);
-    recMergeSort(arr, mid+1, high);
-    mergeArrs(arr,low,mid,high);
-    
-}
-
-void mergeArrs(vector<int>& A, int low, int mid,int high){
-    vector<int> aux(high,-1);
-    for (int i = low; i <= high; i++){
-        aux[i] = A[i];
+    vector<int> a = {9,8,7,6,5};
+    int hi = int(a.size()) - 1;
+    recMergeSort(a, 0, hi);
+    cout << "Finally: " <<endl;
+    for (auto i : a){
+        cout << i << " ";
     }
     cout << endl;
-    cout << "************** Arr received in mergeArrs is: ****************" << endl;
-    for (auto j : aux)
-        cout << j << " ";
-    cout << "*********** done ************ " << endl;
-    cout << endl;
+}
 
+void recMergeSort(vector<int>& arr,int lo, int hi){
+    if (hi == lo)
+        return;
+    int mid = (lo + (hi - lo)/2);
+    recMergeSort(arr, lo, mid);
+    recMergeSort(arr, mid+1, hi);
+    merge(arr,lo,mid,hi);
+}
+
+void merge(vector<int>& originalArr,int lo, int mid, int hi){
+    vector<int> temp(originalArr);
+    int i = lo;
+    int j = mid + 1;
+    int k = lo;
+    
+    for (; k <= hi; k++){
+        if ((i <= mid) && temp[i] < temp[j]){
+            originalArr[k] = temp[i];
+            i++;
+        } else if ((j <= hi) && temp[j] < temp[i]){
+            originalArr[k] = temp[j];
+            j++;
+        } else if (j > hi){
+            originalArr[k] = temp[i];
+            i++;
+        } else {
+            originalArr[k] = temp[j];
+            j++;
+        }
+    }
+    
+    cout << "Returning from merge: " << endl;
+    for (auto i : originalArr)
+        cout << i << " ";
+    cout << endl;
+    cout << endl;
 }
